1731cac25c1368731adf4082a5b0233100bf318d
yaminbd <binamin183@gmail.com>
2023-04-04 03:34:20 +0100

    express & gitignore

--- Boring/index.js
+++ Boring/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+
+/* Create express instance */
+const express = require("express")
+const app = express();
+const port = 3000;
+
+
+/* Landing route */
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Sample API route
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+
+// Run server!
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
