95c3d69e57f055dfec8469834451c9d554706196
yaminbd <binamin183@gmail.com>
2023-04-13 02:42:48 +0100

    Added Capital City

---
+++ src/models/capital_cities.mjs
@@ -0,0 +1,14 @@
+export default class Capital_cities {
+   
+    name;
+    continent;
+    region;
+    
+  
+    constructor(name, continent, region, ) {
+      this.name = name;
+      this.continent = continent;
+      this.region = region;
+    }
+  }
+  
